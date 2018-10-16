75eb2f7a33e913f8ee9dff299caf9f252d8503b1
majaku <m.jakubowski4git@gmail.com>
2018-10-16 17:12:45 +0200

    completed robot_name exercise

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,22 @@
+from string import ascii_letters as letters
+import random
 class Robot(object):
+    used_names = set()
+
     def __init__(self):
-        pass
+        self.name = Robot.generate_name()
+    def reset(self):
+        self.name = Robot.generate_name()
+        
+    def generate_name():
+        while True:
+            prefix = random.choice(letters) + random.choice(letters)
+            prefix = prefix.upper()
+            number = random.randint(100, 999)       
+            name = prefix + str(number)
+            if not name in Robot.used_names:
+                break
+        
+        Robot.used_names.add(name)
+        
+        return name
