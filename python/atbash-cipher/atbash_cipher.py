14c2370d3c4f7c7a81e05b6c4437b8a415e97f44
majaku <m.jakubowski4git@gmail.com>
2018-11-01 10:31:03 +0100

    stage data before disk format

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,28 @@
+import string
+
+# create mapping from normal alphabet to cipher
+letters = list(string.ascii_lowercase)
+key = letters[:]
+key.reverse()
+
+encode_map = dict(zip(letters, key))
+decode_map = dict(zip(key, letters))
+
+
 def encode(plain_text):
-    pass
+    plain_text = plain_text.lower()
+    ciphered_text = ''
+    for letter in plain_text:
+        if letter.isalpha():
+            ciphered_text += encode_map[letter]
+        else:
+            ciphered_text += letter
+
+    ciphered_text = list(ciphered_text)
+    ciphered_text.reverse()
+    ciphered_text = ''.join(ciphered_text)
+    return ciphered_text
 
 
 def decode(ciphered_text):
-    pass
+    return encode(ciphered_text)
--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,29 @@
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        self.scores = scores[:]
+        self.sorted_scores = scores[:]
+        self.sorted_scores.sort()
+        self.sorted_scores.reverse()
+
+    def latest(self):
+        return self.scores[-1]
+
+    def top(self):
+        return self.sorted_scores[:3]
+
+    def highest(self):
+        return self.sorted_scores[0]
+
+    def report(self):
+        last = self.scores[-1]
+        best = self.sorted_scores[0]
+        if last == best:
+            return "Your latest score was {}. That's your personal best!".format(best)
+        else:
+            return "Your latest score was {}. That's {} short of your personal best!".format(last, (best - last))
+
+
+
+
+
+
